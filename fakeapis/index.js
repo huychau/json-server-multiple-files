cbd3ea0969a7b3906b35651b5aefa66ca4020452
Huy Chau <huy.chau@asnet.com.vn>
2018-06-11 15:19:46 +0700

    Init fakeapis

---
+++ fakeapis/index.js
@@ -0,0 +1,9 @@
+const users = require('./users.json');
+const posts = require('./posts.json');
+// Something more
+
+module.exports = () => ({
+  users: users,
+  posts: posts
+  // Something more
+});
